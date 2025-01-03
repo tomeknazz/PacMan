1096a1158efba1cb900a3cc31d98215cbca836d4
Tomasz Nazar <tomeknazz@gmail.com>
2025-01-03 20:09:03 +0100

    Made maps as functions

--- PacMan.cpp
+++ PacMan.cpp
@@ -1,8 +1,3 @@
-
-
-
-
-
 //dzialajacy labirynt 
 
 #include <SFML/Graphics.hpp>
@@ -10,7 +5,7 @@
 #include <SFML/System.hpp>
 #include<windows.h>
 #include<iostream>
-#include <cstdlib> // Include for rand()
+#include <cstdlib>
 
 constexpr double SPEED = 0.05;
 constexpr int WIDTH = 1260;
@@ -18,45 +13,39 @@ constexpr int HEIGHT = 720;
 using namespace std;
 using namespace sf;
 
-
-
-
-
-class Wall {
+class wall {
 public:
 	RectangleShape shape;
 
-	Wall(float x, float y, float width = 100.f, float height = 40.f) {
+	wall(const float x, const float y, const float width = 100.f, const float height = 40.f) {
 		shape.setSize(Vector2f(width, height));
 		shape.setFillColor(Color::Blue);
 		shape.setPosition(x, y);
 	}
-
-	bool checkCollision(const FloatRect& boundingBox) const {
-		return shape.getGlobalBounds().intersects(boundingBox);
+	bool check_collision(const FloatRect& bounding_box) const {
+		return shape.getGlobalBounds().intersects(bounding_box);
 	}
-
 	void draw(RenderWindow& window) const {
 		window.draw(shape);
 	}
 };
 
-class Labirynth {
+class labirynth {
 private:
-	std::vector<Wall> walls;
+	std::vector<wall> walls;
 
 public:
-	Labirynth() {}
+	labirynth() {}
 
-	// Dodaje œcianê do labiryntu
-	void addWall(float x, float y, float width, float height) {
+	// Dodaje sciane do labiryntu (x,y,width,height)
+	void add_wall(float x, float y, float width, float height) {
 		walls.emplace_back(x, y, width, height);
 	}
 
-	// Sprawdza kolizjê z dowoln¹ œcian¹ w labiryncie
-	bool checkCollision(const FloatRect& boundingBox) const {
+	// Sprawdza kolizje z dowolna sciana w labiryncie
+	bool check_collision(const FloatRect& bounding_box) const {
 		for (const auto& wall : walls) {
-			if (wall.checkCollision(boundingBox)) {
+			if (wall.check_collision(bounding_box)) {
 				return true;
 			}
 		}
@@ -70,11 +59,10 @@ public:
 		}
 	}
 
-	const std::vector<Wall>& getWalls() const {
+	const std::vector<wall>& get_walls() const {
 		return walls;
 	}
 };
-
 
 class pacman
 {
@@ -147,9 +135,9 @@ public:
 
 
 	}
-	void handleCollisionWithWall(const Labirynth& labirynth) {
-		for (const auto& wall : labirynth.getWalls()) {
-			if (wall.checkCollision(getBoundingBox())) {
+	void handle_collision_with_wall(const labirynth& labirynth) {
+		for (const auto& wall : labirynth.get_walls()) {
+			if (wall.check_collision(get_bounding_box())) {
 				// Cofnij ruch w przypadku kolizji
 				if (m.left) p.x += speed;
 				if (m.right) p.x -= speed;
@@ -160,7 +148,7 @@ public:
 			}
 		}
 	}
-	FloatRect getBoundingBox() const {
+	FloatRect get_bounding_box() const {
 		CircleShape shape(20.f); // Rozmiar PacMana
 		shape.setPosition(p.x, p.y);
 		return shape.getGlobalBounds();
@@ -199,7 +187,7 @@ public:
 		p.y = y;
 		color1 = color;
 	}
-	void changeDirection() {
+	void change_direction() {
 		// Losowa zmiana kierunku o 90 stopni
 		int direction = rand() % 4;
 		if (direction == 0) {
@@ -220,45 +208,37 @@ public:
 		}
 	}
 
-	FloatRect getBoundingBox() const {
+	FloatRect get_bounding_box() const {
 		CircleShape shape(20.f);
 		shape.setPosition(p.x, p.y);
 		return shape.getGlobalBounds();
 	}
 
-	void move(const pacman pacman, const Wall& wall)
+	void move(const pacman pacman, const wall& wall)
 	{
-		if (wall.checkCollision(getBoundingBox())) {
-			changeDirection(); // Zmiana kierunku po kolizji
+		if (wall.check_collision(get_bounding_box())) {
+			change_direction(); // Zmiana kierunku po kolizji
 		}
 		if (static_cast<int>(floor(sqrt(pow(pacman.get_position().x, 2) + pow(pacman.get_position().y, 2)))) % 100 == 0) {
-			float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX); // Generate a random float between 0 and 1
+			const float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX); // Generate a random float between 0 and 1
 			if (r < 0.25) {
 				m.down = true;
-				m.left = false;
-				m.right = false;
-				m.up = false;
+				m.right = m.up = m.down = false;
 			}
 			else if (r < 0.5 && r >= 0.25)
 			{
 				m.left = true;
-				m.down = false;
-				m.right = false;
-				m.up = false;
+				m.right = m.up = m.down = false;
 			}
 			else if (r < 0.75 && r >= 0.5)
 			{
 				m.right = true;
-				m.down = false;
-				m.left = false;
-				m.up = false;
+				m.left = m.up = m.down = false;
 			}
 			else
 			{
 				m.up = true;
-				m.down = false;
-				m.left = false;
-				m.right = false;
+				m.left = m.right = m.down = false;
 			}
 		}
 		if (get_position().x <= 1) p.x += SPEED / 2.;
@@ -284,6 +264,9 @@ public:
 	}
 };
 
+void map1(labirynth& l);
+void map2(labirynth& l);
+void map3(labirynth& l);
 
 int main() {
 	RenderWindow window(VideoMode(WIDTH, HEIGHT), "PacMan");
@@ -295,63 +278,9 @@ int main() {
 	ghost g4(570, 400, Color::Magenta);
 
 	// Tworzenie labiryntu
-	Labirynth labirynth;
-	
-	//ramki
-	labirynth.addWall(0, 0, 1260, 30); // Œciana pozioma gorna
-	labirynth.addWall(0, 30, 30, 660); // Œciana pionowa lewa
-	labirynth.addWall(0, 690, 1260, 30); // Œciana pozioma dolna
-	labirynth.addWall(1230, 30, 30, 660); // Œciana pionowa prawa
+	labirynth labirynth;
 
-	//Mapa 1
-	
-	//4 rogi
-	//lewy gora
-	labirynth.addWall(110, 110, 30, 200); //pionowa
-	labirynth.addWall(140, 110, 80, 30); //pozioma
-	//lewy dol
-	labirynth.addWall(110, 410, 30, 200);//pionowa
-	labirynth.addWall(140, 580, 80, 30);//pozioma
-	//prawy gora
-	labirynth.addWall(1120, 110, 30, 200);//pionowa
-	labirynth.addWall(1040, 110, 80, 30);//pozioma
-	
-	//prawy dol
-	labirynth.addWall(1120, 410, 30, 200); //pionowa
-	labirynth.addWall(1040, 580, 80, 30); //pozioma
-	
-
-	//4 bolce pionowe
-	labirynth.addWall(300, 30, 30, 110); //lewy gora
-	labirynth.addWall(930, 30, 30, 110); //prawy gora
-	labirynth.addWall(300, 580, 30, 110); //lewy dol
-	labirynth.addWall(930, 580, 30, 110); //prawy dol
-
-	//6 bolce poziome
-	labirynth.addWall(220, 230, 110, 30); //lewy gorny
-	labirynth.addWall(220, 345, 190, 30); //lewy srodek
-	labirynth.addWall(220, 460, 110, 30); //lewy dol
-	labirynth.addWall(930, 230, 110, 30); //prawy gora
-	labirynth.addWall(850, 345, 190, 30); //prawy srodek
-	labirynth.addWall(930, 460, 110, 30); //prawy dol
-
-	//2 dlugie pionowe
-	labirynth.addWall(410, 230, 30, 260);//lewy
-	labirynth.addWall(820,230,30,260);//prawy
-
-	//2 dlugie poziome
-	labirynth.addWall(410, 110, 440, 30);//gorny
-	labirynth.addWall(410,580,440,30);//dolny
-
-	//srodek
-	labirynth.addWall(520, 140, 30, 170);//pionowy gorny
-	labirynth.addWall(520, 410, 30, 170);//pionowy dolny
-	labirynth.addWall(630, 230, 30, 260);//pionowy srodek
-	labirynth.addWall(660, 230, 80, 30);//poziomy gora
-	labirynth.addWall(660, 345, 80, 30);//poziomy srodek
-	labirynth.addWall(660, 460, 80, 30);//poziomy dol
-	
-
+	map1(labirynth);
 
 	while (window.isOpen()) {
 		Event event;
@@ -362,22 +291,22 @@ int main() {
 
 		// Ruch PacMana
 		p.move();
-		p.handleCollisionWithWall(labirynth);
+		p.handle_collision_with_wall(labirynth);
 
 		// Ruch duszków z uwzglêdnieniem kolizji z labiryntem
-		for (const auto& wall : labirynth.getWalls()) {
-			if (wall.checkCollision(g.getBoundingBox())) g.changeDirection();
-			if (wall.checkCollision(g1.getBoundingBox())) g1.changeDirection();
-			if (wall.checkCollision(g2.getBoundingBox())) g2.changeDirection();
-			if (wall.checkCollision(g3.getBoundingBox())) g3.changeDirection();
-			if (wall.checkCollision(g4.getBoundingBox())) g4.changeDirection();
+		for (const auto& wall : labirynth.get_walls()) {
+			if (wall.check_collision(g.get_bounding_box())) g.change_direction();
+			if (wall.check_collision(g1.get_bounding_box())) g1.change_direction();
+			if (wall.check_collision(g2.get_bounding_box())) g2.change_direction();
+			if (wall.check_collision(g3.get_bounding_box())) g3.change_direction();
+			if (wall.check_collision(g4.get_bounding_box())) g4.change_direction();
 		}
 
-		g.move(p, Wall(0, 0, 0, 0));  // Duszki u¿ywaj¹ swojej logiki
-		g1.move(p, Wall(0, 0, 0, 0)); // U¿ycie pustego walla, bo logika jest w g³ównym
-		g2.move(p, Wall(0, 0, 0, 0));
-		g3.move(p, Wall(0, 0, 0, 0));
-		g4.move(p, Wall(0, 0, 0, 0));
+		g.move(p, wall(0, 0, 0, 0));  // Duszki u¿ywaj¹ swojej logiki
+		g1.move(p, wall(0, 0, 0, 0)); // U¿ycie pustego walla, bo logika jest w g³ównym
+		g2.move(p, wall(0, 0, 0, 0));
+		g3.move(p, wall(0, 0, 0, 0));
+		g4.move(p, wall(0, 0, 0, 0));
 
 		// Rysowanie
 		window.clear();
@@ -394,3 +323,59 @@ int main() {
 	return 0;
 }
 
+void map1(labirynth& l)
+{
+	//ramki
+	l.add_wall(0, 0, 1260, 30); // Œciana pozioma gorna
+	l.add_wall(0, 30, 30, 660); // Œciana pionowa lewa
+	l.add_wall(0, 690, 1260, 30); // Œciana pozioma dolna
+	l.add_wall(1230, 30, 30, 660); // Œciana pionowa prawa
+
+	//Mapa 1
+
+	//4 rogi
+	//lewy gora
+	l.add_wall(110, 110, 30, 200); //pionowa
+	l.add_wall(140, 110, 80, 30); //pozioma
+	//lewy dol
+	l.add_wall(110, 410, 30, 200);//pionowa
+	l.add_wall(140, 580, 80, 30);//pozioma
+	//prawy gora
+	l.add_wall(1120, 110, 30, 200);//pionowa
+	l.add_wall(1040, 110, 80, 30);//pozioma
+
+	//prawy dol
+	l.add_wall(1120, 410, 30, 200); //pionowa
+	l.add_wall(1040, 580, 80, 30); //pozioma
+
+
+	//4 bolce pionowe
+	l.add_wall(300, 30, 30, 110); //lewy gora
+	l.add_wall(930, 30, 30, 110); //prawy gora
+	l.add_wall(300, 580, 30, 110); //lewy dol
+	l.add_wall(930, 580, 30, 110); //prawy dol
+
+	//6 bolce poziome
+	l.add_wall(220, 230, 110, 30); //lewy gorny
+	l.add_wall(220, 345, 190, 30); //lewy srodek
+	l.add_wall(220, 460, 110, 30); //lewy dol
+	l.add_wall(930, 230, 110, 30); //prawy gora
+	l.add_wall(850, 345, 190, 30); //prawy srodek
+	l.add_wall(930, 460, 110, 30); //prawy dol
+
+	//2 dlugie pionowe
+	l.add_wall(410, 230, 30, 260);//lewy
+	l.add_wall(820, 230, 30, 260);//prawy
+
+	//2 dlugie poziome
+	l.add_wall(410, 110, 440, 30);//gorny
+	l.add_wall(410, 580, 440, 30);//dolny
+
+	//srodek
+	l.add_wall(520, 140, 30, 170);//pionowy gorny
+	l.add_wall(520, 410, 30, 170);//pionowy dolny
+	l.add_wall(630, 230, 30, 260);//pionowy srodek
+	l.add_wall(660, 230, 80, 30);//poziomy gora
+	l.add_wall(660, 345, 80, 30);//poziomy srodek
+	l.add_wall(660, 460, 80, 30);//poziomy dol
+}
